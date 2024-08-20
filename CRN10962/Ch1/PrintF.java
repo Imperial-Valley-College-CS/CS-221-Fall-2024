2555326f8c11b60bd14e4de255759e0df8e46ee2
ortoct <104336410+ortoct@users.noreply.github.com>
2024-08-20 15:34:34 -0700

    PA1 files

---
+++ CRN10962/Ch1/PrintF.java
@@ -0,0 +1,10 @@
+package PA1_Print.src.main.java;
+
+public class PrintF
+{
+   public static void main(String[] args)
+   {         
+      System.out.printf("Favorite Color: %s%nCurrent Weight: %f%nFirst Initial: %c%n",
+         "green", 180.8, 'O');
+   }
+}
