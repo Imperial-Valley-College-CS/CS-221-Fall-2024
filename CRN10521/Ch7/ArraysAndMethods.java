ed8781b329e168f4ec8cf9e0bc799aff8f9f11fc
ortoct <104336410+ortoct@users.noreply.github.com>
2024-09-25 10:01:51 -0700

    ArraysAndMethods

--- CRN10521/Ch7/ArraysAndMethods.java
+++ CRN10521/Ch7/ArraysAndMethods.java
@@ -26,4 +26,32 @@ public class ArraysAndMethods
    public static boolean[] startsWith( String[] names, char letter )
    {
       boolean[] returnArray = new boolean[names.length];
-      for( int
+      for( int i = 0; i < names.length; i++ )
+      {
+         if( names[i].charAt(0) == letter )
+         {
+            returnArray[i] = true;
+         }else
+         {
+            returnArray[i] = false;
+         }
+      }//end for
+      return returnArray;
+   }//end startsWith
+   
+   public static double gpa( char[] g )
+   {
+      double sum = 0;
+      for( char x : g )
+      {
+         switch(x)
+         {
+            case 'A': case 'a': sum += 4; break;
+            case 'B': case 'b': sum += 3; break;
+            case 'C': case 'c': sum += 2; break;
+            case 'D': case 'd': sum += 1; break;
+         }
+      }
+      return sum/g.length;
+   }
+}
