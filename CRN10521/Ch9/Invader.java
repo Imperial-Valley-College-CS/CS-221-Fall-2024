7293767d62eb7982386a2f3100e846545252b071
ortoct <104336410+ortoct@users.noreply.github.com>
2024-10-14 09:55:29 -0700

    Updated Invader

--- CRN10521/Ch9/Invader.java
+++ CRN10521/Ch9/Invader.java
@@ -1,5 +1,49 @@
-import javafx.application.Application;
+import javafx.scene.paint.Color;
+import java.io.*;
+import java.util.Scanner;
 
 public class Invader
 {
+   //data fields 
+   private boolean[][] body = new boolean[8][12];
+   private Color color;
+   private String filename = System.getProperty("user.dir") + "/InvaderMatrices/";
+   //private Position pos;
+   
+   public Invader(String type, int x, int y)
+   {
+      //this.pos = new Position(x,y);
+      System.out.println( filename );
+      if( type.equals("octopus") )
+      {
+         this.filename += "OctopusMatrix.txt";
+         this.color = Color.rgb(196,22,248);    //purple
+      }
+   }
+   
+   public void setBody()
+   {
+      try
+      {
+         File f = new File(this.filename);
+         Scanner scan = new Scanner(f);
+         int i = 0;
+         while( scan.hasNextLine() )
+         {
+            String[] line = scan.nextLine().split(",");
+            for( int j = 0; j < line.length; j++ )
+            {
+               if( line[j].equals("1") )
+                  body[i][j] = true;
+               else
+                  body[i][j] = false;
+            }
+            i++;
+         }
+      }catch(FileNotFoundException e)
+      {
+         System.out.println( "File not found." );
+         body = null;
+      }
+   }
 }
---
+++ CRN10521/Ch9/TestInvader.java
@@ -0,0 +1,13 @@
+public class TestInvader
+{
+   public static void main(String[] args)
+   {
+      Invader invader = new Invader("octopus", 0, 0);
+      // boolean[][] body = invader.getBody();
+//       double x = invader.getPosition().getX();
+//       double y = invader.getPosition().getY();
+//       System.out.println( "Position - x: " + x + " y: " + y );     
+//       for( boolean[] row : body )
+//          System.out.println( Arrays.toString( row ) );
+   }
+}
