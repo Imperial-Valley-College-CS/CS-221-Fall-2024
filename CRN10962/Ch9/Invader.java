070cec7cde35447d14bc1bfffe6315a4737bb5c6
ortoct <104336410+ortoct@users.noreply.github.com>
2024-10-15 16:07:03 -0700

    Updated getBody

--- CRN10962/Ch9/Invader.java
+++ CRN10962/Ch9/Invader.java
@@ -1,3 +1,38 @@
+import javafx.scene.paint.Color;
+import java.io.*;
+import java.util.Scanner;
+
 public class Invader
 {
+   //data fields
+   private int health, speedX, speedY;
+   private boolean[][] body = new boolean[8][12];
+   //private Position position;
+   private Color color;
+   
+   public void setBody()
+   {
+      try
+      {
+         File f = new File("InvaderMatrices/CrabMatrix.txt");
+         Scanner scan = new Scanner(f);
+         int i = 0;
+         while( scan.hasNextLine() )
+         {
+            String[] line = scan.nextLine().split(",");
+            for( int j = 0; j < line.length; j++ )
+            {
+               if( line[j].equals("0") )
+                  body[i][j] = false;
+               else
+                  body[i][j] = true;
+            }
+            i++;
+         }
+      }catch(FileNotFoundException e)
+      {
+         System.out.println( "File not found." );
+         body = null;
+      }
+   }   
 }
---
+++ CRN10962/Ch9/TestInvader.java
@@ -0,0 +1,8 @@
+public class TestInvader
+{
+   public static void main(String[] args)
+   {
+      Invader vader = new Invader();
+      vader.setBody();
+   }
+}
