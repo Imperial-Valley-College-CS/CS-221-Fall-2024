97bd6367e51feadc378595aed7e45749639d394e
ortoct <104336410+ortoct@users.noreply.github.com>
2024-11-04 09:33:27 -0800

    Updated MouseHandler

--- CRN10521/JavaFX/SnakeGame/App.java
+++ CRN10521/JavaFX/SnakeGame/App.java
@@ -2,19 +2,21 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.event.EventHandler;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.input.*;
 
 public class App extends Application
 {
    Group g = new Group();
    Scene scene = new Scene(g, Constants.WIDTH, Constants.HEIGHT, Constants.bgColor);
    KeyHandler handleKey = new KeyHandler();
+   MouseHandler handleMouse = new MouseHandler();
    
    //must override inherited abstract methods
    @Override
    public void start(Stage s)
    {
-      scene.setOnKeyPressed(handleKey);      //make keyboard come to live (needs EventHandler)
+      scene.setOnMouseClicked( handleMouse );   //makes mouse come to live (needs EventHandler<MouseEvent>)
+      scene.setOnKeyPressed(handleKey);      //make keyboard come to live (needs EventHandler<KeyEvent>)
       s.setScene( scene );
       s.show();
    }
@@ -24,10 +26,20 @@ public class App extends Application
    {
       @Override
       public void handle(KeyEvent e)      //invoked when key is pressed (KeyEvent)         
-      {
+      {  
          System.out.println( e.getCode().toString() );
       }
    }
    
+   //inner class
+   class MouseHandler implements EventHandler<MouseEvent>
+   {
+      @Override
+      public void handle(MouseEvent e)      //invoked when mouse is activated (MouseEvent)        
+      {
+         System.out.flush();
+         System.out.println( "X: " + e.getSceneX() + ", Y: " + e.getSceneY());
+      }
+   }
    
 }//App ends
