e09c7f071c50e991d39d51c723b2d1f38a19abfc
ortoct <104336410+ortoct@users.noreply.github.com>
2024-11-04 09:07:31 -0800

    Updated KeyHandler class

--- CRN10521/JavaFX/SnakeGame/App.java
+++ CRN10521/JavaFX/SnakeGame/App.java
@@ -8,11 +8,13 @@ public class App extends Application
 {
    Group g = new Group();
    Scene scene = new Scene(g, Constants.WIDTH, Constants.HEIGHT, Constants.bgColor);
+   KeyHandler handleKey = new KeyHandler();
    
    //must override inherited abstract methods
    @Override
    public void start(Stage s)
    {
+      scene.setOnKeyPressed(handleKey);      //make keyboard come to live (needs EventHandler)
       s.setScene( scene );
       s.show();
    }
@@ -21,8 +23,9 @@ public class App extends Application
    class KeyHandler implements EventHandler<KeyEvent>
    {
       @Override
-      public void handle(KeyEvent e)
+      public void handle(KeyEvent e)      //invoked when key is pressed (KeyEvent)         
       {
+         System.out.println( e.getCode().toString() );
       }
    }
    
